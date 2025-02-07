56f874829f57e24dbe3138f6e9bae3d4cfe31df5
ArielKings <arielkings1508@gmail.com>
2025-02-07 02:03:46 -0400

    Create Program.cs

---
+++ Tarea #2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Desarrollar un programa que le solicite al usuario un numero y el programa indique si es par o impar.
+        Console.WriteLine("Escribe un numero para determinar si es par o impar: ");
+        int number = Console.Read();
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("El numero que usted ha ingresado es par.");
+        }
+        else
+        {
+            Console.WriteLine("El numero que usted ha ingresado es impar.");
+        }
+    }
+}
